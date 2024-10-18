1391ffb478b60434fb5a7ecaf0dea326783203b2
zipang <zzp-email@qq.com>
2024-10-18 14:58:05 +0800

    do while -> while

--- week4/Lab7E6b.java
+++ week4/Lab7E6b.java
@@ -3,6 +3,7 @@ package week4;
 public class Lab7E6b {
     public static void main(String[] args) {
         int i = 5;
+        System.out.println( i--);
         while (i >= 0) {
             System.out.println( i--);
         }
