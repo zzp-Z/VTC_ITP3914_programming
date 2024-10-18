488d4a25dcd2876198f767ba3bb75c3af5f905e1
zipang <zzp-email@qq.com>
2024-10-18 14:44:34 +0800

    Lab7E6a and Lab7E6b.java

---
+++ week4/Lab7E6a.java
@@ -0,0 +1,13 @@
+package week4;
+
+public class Lab7E6a {
+    public static void main(String[] args) {
+        int i = -9;
+        while (i <= -1) {
+            System.out.println(i);
+            i += 3;
+        }
+        System.out.println("After loop, i=" + i);
+
+    }
+}
---
+++ week4/Lab7E6b.java
@@ -0,0 +1,11 @@
+package week4;
+
+public class Lab7E6b {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i >= 0) {
+            System.out.println( i--);
+        }
+        System.out.println("After loop, i=" + i);
+    }
+}
